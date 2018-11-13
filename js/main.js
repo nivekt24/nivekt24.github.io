ef261ecc834f4a9823e6d5c7b45e2890a3c1ee96
Kevin <kevinntrann@gmail.com>
2018-11-13 14:58:56 -0800

    portfolio update

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
+'use strict';
+
 let toggleNavStatus = false;
 
 let toggleNav = function() {
   let getMobileNav = document.querySelector('.mobile-nav');
   let getMobileNavUl = document.querySelector('.mobile-nav__items');
-  let getMobileNavLinks = document.querySelectorAll('.mobile-nav__item a');
+  let getMobileNavLinks = document.querySelectorAll('.mobile-nav__item');
 
   if (toggleNavStatus === false) {
     getMobileNav.style.height = '250px';
@@ -11,11 +13,21 @@ let toggleNav = function() {
     getMobileNav.style.zIndex = '1000';
     getMobileNav.style.top = '50px';
 
+    let arrayLength = getMobileNavLinks.length;
+    for (let i = 0; i < arrayLength; i++) {
+      getMobileNavLinks[i].style.opacity = '1';
+    }
+
     toggleNavStatus = true;
   } else if (toggleNavStatus === true) {
     getMobileNav.style.height = '0';
     getMobileNav.style.top = '0';
     getMobileNav.style.position = 'static';
+
+    let arrayLength = getMobileNavLinks.length;
+    for (let i = 0; i < arrayLength; i++) {
+      getMobileNavLinks[i].style.opacity = '0';
+    }
     toggleNavStatus = false;
   }
 };
